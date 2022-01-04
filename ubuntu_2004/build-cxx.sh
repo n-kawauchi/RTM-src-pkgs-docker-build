7403e163baee10eaf9a79e2b79c2caf4fda1f05a
n-kawauchi <n.kawauchi@aist.go.jp>
2022-01-04 14:27:35 +0900

    Updated c++ scripts for OpenRTM2.0.

--- ubuntu_2004/build-cxx.sh
+++ ubuntu_2004/build-cxx.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-CODE_NAME=focal
+UBUNTU_VER=2004
 TARGET=cxx
-VERSION=1.2.2
-BRANCH=svn/RELENG_1_2
+VERSION=2.0.0
 SHORT_VER=`echo $VERSION | cut -b 1-3 | sed 's/\.//g'`
-#REPO=openrtm.org
-REPO=150.29.99.185
 
 printf "sudo password: "
 stty -echo
@@ -18,19 +15,11 @@ echo "${password}" | sudo -S rm -rf ${TARGET}-*
 rm -rf OpenRTM-aist
 
 git clone https://github.com/OpenRTM/OpenRTM-aist
-cd OpenRTM-aist
-git checkout ${BRANCH} 
-cd -
 
 # build in docker environment
 echo "${password}" | sudo -S docker build \
- --build-arg CODE_NAME=${CODE_NAME} \
- --build-arg TARGET=${TARGET} \
- --build-arg VERSION=${VERSION} \
- --build-arg REPO=${REPO} \
  -t ${TARGET}${SHORT_VER} \
- -f Dockerfile-${TARGET}-${SHORT_VER}-deb .
+ -f OpenRTM-aist/scripts/ubuntu_${UBUNTU_VER}/Dockerfile.package .
 echo "${password}" | sudo -S docker create --name ${TARGET}${SHORT_VER} ${TARGET}${SHORT_VER}
-echo "${password}" | sudo -S docker cp ${TARGET}${SHORT_VER}:/root/${TARGET}-src-pkgs .
 echo "${password}" | sudo -S docker cp ${TARGET}${SHORT_VER}:/root/${TARGET}-deb-pkgs .
 echo "${password}" | sudo -S docker rm ${TARGET}${SHORT_VER}
